2e084f7ab7a86419eb4bb1ad07cf84941b7f73f4
alstonlo <40709307+alstonlo@users.noreply.github.com>
2021-01-24 16:58:32 -0500

    new dataset

--- datasets/qm9/process_qm9.py
+++ datasets/qm9/process_qm9.py
@@ -1,9 +1,10 @@
 import pathlib
 
+import deepsmiles
+import pandas as pd
 import selfies as sf
 import tqdm
 from rdkit import Chem
-import pandas as pd
 
 QM9_DIR = pathlib.Path(__file__).parent
 
@@ -11,16 +12,14 @@ constraints = sf.get_semantic_constraints()
 constraints['N'] = 6
 sf.set_semantic_constraints(constraints)
 
-
-def mol_to_smiles(mol) -> str:
-    return Chem.MolToSmiles(mol, kekuleSmiles=True, canonical=True)
+converter = deepsmiles.Converter(rings=True, branches=True)
 
 
 def main():
     with open(QM9_DIR / 'raw_qm9.txt', 'r') as f:
         lines = list(map(lambda s: s.rstrip('\n'), f.readlines()))
 
-    columns = ['smiles', 'selfies', 'num_heavy_atoms']
+    columns = ['smiles', 'deep_smiles', 'selfies', 'num_heavy_atoms']
     qm9_rows = []
 
     for smiles in tqdm.tqdm(lines, desc="Reading SMILES"):
@@ -28,8 +27,15 @@ def main():
         mol = Chem.MolFromSmiles(smiles)
         num_heavy_atoms = mol.GetNumHeavyAtoms()
 
+        deep_smiles = converter.encode(smiles)
         selfies = sf.encoder(smiles)
-        qm9_rows.append((smiles, selfies, num_heavy_atoms))
+        qm9_rows.append((smiles, deep_smiles, selfies, num_heavy_atoms))
+
+        # sanity check
+        try:
+            converter.decode(deep_smiles)
+        except deepsmiles.DecodeError as e:
+            print("DecodeError! Error message was '%s'" % e.message)
 
     qm9_df = pd.DataFrame(qm9_rows, columns=columns)
     qm9_df.to_csv(QM9_DIR / 'qm9.csv', index=False)
